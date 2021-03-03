d202790f1a55dcdb286ac9c600d9a4c53bd7d9cb
tiennt <tiennt.june.20@gmail.com>
2021-03-03 23:21:47 +0700

    add address table

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(128))
   age = db.Column(db.Integer)
+
+
+class Address(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  city = db.Column(db.String(128))
